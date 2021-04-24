aeb3240b1f1e2ae23ac1a8fef0ed66bd3a5dfaa1
chenyaozong <18652844803@163.com>
2021-04-24 23:18:05 +0800

    Update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,13 @@
 module.exports = {
-    title: 'luke chan"s blog',
+    title: 'luke chan\'s blog',
     theme: '@vuepress/blog',
+    themeConfig: {
+        // 请参考文档来查看所有可用的选项。
+        nav: [
+            { text: 'front-end', link: 'https://google.com', target:'_self', rel:'' },
+            { text: '区块链', link: '/guide/', target:'_blank' }
+        ]
+    },
     description: 'Just playing around',
-    base: 'github_blog',
+    base: '/github_blog/',
 }
